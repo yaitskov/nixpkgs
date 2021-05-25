9080c9bc92861260a8dbda59e9de3967eeef1c3f
Daniil Iaitskov <daniil.iaitskov@soostone.com>
2021-05-25 13:43:57 -0400

    pass-CI_COMMON_SHA filter generated default nix

--- pkgs/development/haskell-modules/make-package-set.nix
+++ pkgs/development/haskell-modules/make-package-set.nix
@@ -123,6 +123,10 @@ let
     let
       sha256Arg = if sha256 == null then "--sha256=" else ''--sha256="${sha256}"'';
       extraVars = (builtins.listToAttrs (builtins.map (envVar: {name = envVar; value = (builtins.getEnv envVar); })  passEnvVars));
+      extraVarsAsEntries = (builtins.concatStringsSep
+                             "\n"
+                             (builtins.map (envVar: (builtins.concatStringsSep "" [ "\"" envVar "\" = \"" (builtins.getEnv envVar) "\";" ]))
+                               passEnvVars));
     in buildPackages.stdenv.mkDerivation ({
       name = "cabal2nix-${name}";
       nativeBuildInputs = [ buildPackages.cabal2nix-unwrapped ];
@@ -134,7 +138,7 @@ let
       installPhase = ''
         export HOME="$TMP"
         mkdir -p "$out"
-        cabal2nix --compiler=${self.ghc.haskellCompilerName} --system=${hostPlatform.config} ${sha256Arg} "${src}" ${extraCabal2nixOptions} > "$out/default.nix"
+        cabal2nix --compiler=${self.ghc.haskellCompilerName} --system=${hostPlatform.config} ${sha256Arg} "${src}" ${extraCabal2nixOptions} | while read a ; do if [[ $a =~ isExecutable ]] ; then echo -e "${extraVarsAsEntries}\n $a" ; else echo "$a" ; fi ; done > "$out/default.nix"
       '';
     } // (builtins.trace
            (builtins.concatStringsSep "" ["PASSED EXTRA VARS "
